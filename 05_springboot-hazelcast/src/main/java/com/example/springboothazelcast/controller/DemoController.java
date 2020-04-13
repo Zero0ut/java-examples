bcf23a829f82ebfaabeca3399fd178b68bdc569d
Alex Cheng <alex.kpc@gmail.com>
2020-04-14 00:05:33 +0800

    Initial Commit

---
+++ 05_springboot-hazelcast/src/main/java/com/example/springboothazelcast/controller/DemoController.java
@@ -0,0 +1,25 @@
+package com.example.springboothazelcast.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/whoami")
+    public String whoami() {
+        StringBuilder message = new StringBuilder("You are connected ");
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            message.append(" from host: " + ip);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return message.toString();
+    }
+
+
+}
